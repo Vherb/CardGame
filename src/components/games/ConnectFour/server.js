b92640a9839d509362f54cd40f8ec69ee5a8ce72
Vherb <victorherbert1995@gmail.com>
2023-12-03 19:44:56 -0600

    fixed a few more bugs

--- src/components/games/ConnectFour/server.js
+++ src/components/games/ConnectFour/server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 const games = new Map();
 let waitingPlayers = [];
+let game; // Define game variable outside of the callback function
 
 const allowedOrigins = ['http://192.168.50.123:3000']; // Update with your allowed origins
 const corsOptions = {
@@ -27,10 +28,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 wss.on('connection', (ws) => {
-  let game;
+  console.log('Client connected'); // Add this line
 
   ws.on('message', (message) => {
     try {
+      console.log('Received message:', message); // Add this line
+
       const data = JSON.parse(message);
 
       switch (data.type) {
@@ -55,28 +58,54 @@ wss.on('connection', (ws) => {
 
               games.set(game.players[0], game);
               games.set(game.players[1], game);
+
+              // Check if two players have joined and the game is ready to start
+              if (game.isFull()) {
+                game.players.forEach((player) => {
+                  player.send(
+                    JSON.stringify({
+                      type: 'startGame',
+                      currentPlayer: game.currentPlayer,
+                    })
+                  );
+                });
+              }
             }
           }
           break;
 
-        case 'makeMove':
-          if (game && game.players.includes(ws) && game.makeMove(data.col)) {
-            game.currentPlayer = game.currentPlayer === 'Player 1' ? 'Player 2' : 'Player 1';
-
-            // Send game update to all players in the game
-            game.players.forEach((player, index) => {
-              player.send(
-                JSON.stringify({
-                  type: 'gameUpdate',
-                  board: game.board,
-                  currentPlayer: game.currentPlayer,
-                  winner: game.winner,
-                  playerNumber: index + 1,
-                })
-              );
-            });
-          }
-          break;
+          case 'makeMove':
+            if (game && game.players.includes(ws)) {
+              const col = data.col;
+          
+              // Attempt to make a move and check if it's valid
+              const validMove = game.makeMove(col);
+          
+              if (validMove) {
+                // Check for a winner here and set the game as over if needed
+                const row = game.findEmptyRow(col); // Find the row where the move was made
+                if (game.checkWin(row, col)) { // Pass row and col to checkWin
+                  game.gameOver = true;
+                }
+          
+                // Send game update to all players in the game
+                game.players.forEach((player) => {
+                  player.send(
+                    JSON.stringify({
+                      type: 'gameUpdate',
+                      board: game.board,
+                      currentPlayer: game.currentPlayer,
+                      winner: game.winner,
+                      playerNumber: player.playerNumber,
+                    })
+                  );
+                });
+              }
+            }
+            break;
+          
+          
+          
 
         default:
           console.error('Invalid message type:', data.type);
@@ -87,11 +116,14 @@ wss.on('connection', (ws) => {
   });
 
   ws.on('close', () => {
+    console.log('Client disconnected'); // Add this line
+
     if (game) {
       game.players = game.players.filter((player) => player !== ws);
       if (game.players.length === 0) {
         games.delete(game.players[0]);
         games.delete(game.players[1]);
+        game = null; // Reset the game if all players have left
       }
     } else {
       waitingPlayers = waitingPlayers.filter((player) => player !== ws);
